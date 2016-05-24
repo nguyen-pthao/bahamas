d02b4cae4f40101bc9b57cdd87a83cf97451f102
Tan Si Hao <sihao.tan.2014@smu.edu.sg>
2016-05-25 00:03:30 +0800

    Load data to tables

---
+++ bahamas/sql/data.sql
@@ -0,0 +1,164 @@
+#Dummy data to be loaded to bahamas tables
+
+#PERMISSION_LEVEL_LIST
+insert into PERMISSION_LEVEL_LIST(PERMISSION, DESCRIPTION) value
+('Team manager', 'Manages a team'),
+('Event leader', 'Manages an event'),
+('Associate', 'Joins event');
+
+#TYPE_OF_CONTACT_LIST
+insert into TYPE_OF_CONTACT_LIST(CONTACT_TYPE) value
+('Individual'),
+('Foundation'),
+('Religious'),
+('Corporate'),
+('Government-linked'),
+('Diplomatic'),
+('Nonprofit local'),
+('Nonprofit abroad'),
+('Medical'),
+('Legal'),
+('Educational'),
+('Media local'),
+('Media abroad'),
+('Financial'),
+('Job agent'),
+('Employer'),
+('Product supplier'),
+('Office services'),
+('Event services'),
+('Financial services'),
+('Maintenance & repair'),
+('TWC2'),
+('Other');
+
+#LANGUAGE_LIST
+insert into LANGUAGE_LIST(LANGUAGE_NAME) value
+('Bengali'),
+('Burmese'),
+('Cambodian'),
+('Chinese'),
+('Filipino'),
+('Hindi'),
+('Indonesian'),
+('Malay'),
+('Punjabi'),
+('Sinhala'),
+('Tamil'),
+('Telugu'),
+('Vietnamese'),
+('Other');
+
+#OFFICE_LIST
+insert into OFFICE_LIST(OFFICE_ID,OFFICE_HELD_NAME,DESCRIPTION) value
+(1,'President','DESCRIPTION of President'),
+(2,'Vice-president','DESCRIPTION of Vice-president'),
+(3,'Secretary','DESCRIPTION of Secretary'),
+(4,'Treasurer','DESCRIPTION of Treasurer'),
+(5,'Staff','DESCRIPTION of Staff');
+
+#MODE_OF_SENDING_RECEIPT_LIST
+insert into MODE_OF_SENDING_RECEIPT_LIST(RECEIPT_MODE_NAME) value
+('Email'),
+('Post'),
+('No address'),
+('Other');
+
+#MEMBERSHIP_CLASS_LIST
+insert into MEMBERSHIP_CLASS_LIST(MEMBERSHIP_CLASS_NAME) value
+('Ordinary'),
+('Discounted'),
+('Associate'),
+('Honorary'),
+('Other');
+
+#PAYMENT_MODE_LIST
+insert into PAYMENT_MODE_LIST(PAYMENT_MODE_NAME) value
+('Cash'),
+('Bank direct'),
+('Cheque'),
+('Paypal'),
+('Giving Sg'),
+('Other');
+
+#LSA_CLASS_LIST
+insert into LSA_CLASS_LIST(SKILL_NAME) value
+('Medical'),
+('Physio'),
+('Pharma'),
+('Dental'),
+('Counselling'),
+('Legal'),
+('Pro-bono lawyer'),
+('Media & Comms'),
+('Infotech'),
+('Photo, video & design'),
+('Accounting'),
+('Academia'),
+('Event organising'),
+('Has event space'),
+('Has vehicle'),
+('Has shelter space'),
+('Other');
+
+#EVENT_CLASS_LIST
+insert into EVENT_CLASS_LIST(EVENT_CLASS_NAME) value
+('Closed'),
+('Open'),
+('Training'),
+('Cancelled'),
+('Other');
+
+#EVENT_LOCATION_LIST
+insert into EVENT_LOCATION_LIST(EVENT_LOCATION_NAME) value
+('TWC2 office'),
+('TWC2 DaySpace'),
+('Isthana'),
+('Alankar'),
+('Online'),
+('Others');
+
+#TEAM_AFFILIATION_LIST
+insert into TEAM_AFFILIATION_LIST(TEAM_NAME, DESCRIPTION) value
+('TCRP','This is TCRP team'),
+('R2R','This is R2R team'),
+('FareGo','This is FareGo team'),
+('Roof','This is Roof team'),
+('Lifeline','This is Lifeline team'),
+('Casework','This is Casework team'),
+('Intern','This is Intern team'),
+('Outreach','This is Outreach team'),
+('Discover Sg','This is Discover Sg team'),
+('Public Engagement','This is Public Engagement team'),
+('Communications','This is Communications team'),
+('Research','This is Research team'),
+('Fundraising','This is Fundraising team'),
+('Other','This is Other team');
+
+
+#CONTACT
+insert into CONTACT(CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,ISADMIN,DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION) value
+(1, 'TWC2', 'admin1', '1234', 'Y', 'N', '2016-05-24', 'admin', 'Thao1', 'Cool Thao1', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(2, 'TWC2', 'admin2', '1234', 'Y', 'N', '2016-05-24', 'admin', 'Thao2', 'Cool Thao2', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(3, 'Religious', 'teammanager1', '1234', 'N', 'N', '2016-05-24', 'admin', 'tan1', 'Cool tan1', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(4, 'Media local', 'teammanager2', '1234', 'N', 'N', '2016-05-24', 'admin', 'tan2', 'Cool tan2', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(5, 'Educational', 'eventleader1', '1234', 'N', 'N', '2016-05-24', 'admin', 'Goh1', 'Cool Goh1', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(6, 'Employer', 'eventleader2', '1234', 'N', 'N', '2016-05-24', 'admin', 'Goh2', 'Cool Goh2', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(7, 'Event services', 'associate1', '1234', 'N', 'N', '2016-05-24', 'admin', 'Mok1', 'Cool Mok1', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(8, 'Government-linked', 'associate2', '1234', 'N', 'N', '2016-05-24', 'admin', 'Mok2', 'Cool Mok2', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(9, 'Legal', 'novice1', '1234', 'N', 'N', '2016-05-24', 'admin', 'Ong1', 'Cool Ong1', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y'),
+(10, 'Medical', 'novice2', '1234', 'N', 'N', '2016-05-24', 'admin', 'Ong2', 'Cool Ong2', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 'y');
+
+#TEAM_JOIN
+insert into TEAM_JOIN(CONTACT_ID, TEAM_NAME, DATE_CREATED, CREATED_BY, EXPLAIN_IF_OTHER, SUBTEAM, DATE_OBSOLETE, REMARKS,PERMISSION) value
+(3, 'Lifeline', '2016-05-24', 'Thao1' , '', '', '', '', 'Team manager'),
+(4, 'Research', '2016-05-24', 'Thao2' , '', '', '', '', 'Team manager'),
+(5, 'Lifeline', '2016-05-24', 'Thao1' , '', '', '', '', 'Event leader'),
+(6, 'Research', '2016-05-24', 'Thao2' , '', '', '', '', 'Event leader'),
+(7, 'Lifeline', '2016-05-24', 'Thao1' , '', '', '', '', 'Associate'),
+(8, 'Research', '2016-05-24', 'Thao2' , '', '', '', '', 'Associate');
+
+
+
+
+
