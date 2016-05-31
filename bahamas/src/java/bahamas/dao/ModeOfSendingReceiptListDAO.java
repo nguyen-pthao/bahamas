56d090a1259d31543b2fd7b3d0ab020debf62f4a
marcus.ong.2014 <marcus.ong.2014@smu.edu.sg>
2016-06-01 01:13:45 +0800

    mode of sending receipt list and dao

---
+++ bahamas/src/java/bahamas/dao/ModeOfSendingReceiptListDAO.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Marcus
+ */
+public class ModeOfSendingReceiptListDAO {
+    
+    private ArrayList<String> modeOfSendingReceiptList;
+    
+
+    public ModeOfSendingReceiptListDAO() {
+    }
+    
+    public ArrayList<String> retrieveMOSRList() {
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        modeOfSendingReceiptList = new ArrayList<String>();
+
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("SELECT * FROM MODE_OF_SENDING_RECEIPT_LIST");
+
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                modeOfSendingReceiptList.add(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ModeOfSendingReceiptListDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve payment mode list from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        return modeOfSendingReceiptList;
+    }
+    
+}
---
+++ bahamas/src/java/bahamas/services/ModeOfSendingReceiptList.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.services;
+
+import bahamas.dao.ModeOfSendingReceiptListDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marcus Ong
+ */
+@WebServlet(name = "ModeOfSendingReceiptList", urlPatterns = {"/ModeOfSendingReceiptList"})
+public class ModeOfSendingReceiptList extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/JSON;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            JsonObject json = new JsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+            /*          
+            String token = request.getParameter("token");
+            if (!Authenticator.verifyToken(token)) {
+                json.addProperty("message", "invalid token");
+                out.println(gson.toJson(json));
+
+            } else {
+                //Verified
+             */
+            //Validation
+            //Create new contact object
+            ModeOfSendingReceiptListDAO modeOfSendingReceiptListDAO = new ModeOfSendingReceiptListDAO();
+            ArrayList<String>  mosrList = modeOfSendingReceiptListDAO.retrieveMOSRList();
+
+            if (!mosrList.isEmpty()) {
+                json.addProperty("message", "success");
+
+                JsonArray mosrArray = new JsonArray();
+                JsonObject jsonMOSRObj;
+
+                for (String s : mosrList) {
+
+                    jsonMOSRObj = new JsonObject();
+                    jsonMOSRObj.addProperty("modeOfSendingReceipt", s);
+                    
+                    mosrArray.add(jsonMOSRObj);
+
+                }
+                json.add("mode", mosrArray);
+                out.println(gson.toJson(json));
+            } else {
+                json.addProperty("message", "failed");
+                out.println(gson.toJson(json));
+            }
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
