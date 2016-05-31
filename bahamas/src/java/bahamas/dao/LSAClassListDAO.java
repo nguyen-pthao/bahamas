e82d1792d53515179a85ecd9513015bff39f8b96
Darryl Mok <darryl.mok.2014@smu.edu.sg>
2016-05-31 23:31:32 +0800

    edited variable name

--- bahamas/src/java/bahamas/dao/LSAClassListDAO.java
+++ bahamas/src/java/bahamas/dao/LSAClassListDAO.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class LSAClassListDAO {
     
-    private ArrayList<String> LSAClassList;
+    private ArrayList<String> lsaClassList;
     
 
     public LSAClassListDAO() {
