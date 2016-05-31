2e65b48676f8eb856350acf6dbaaa0c9fb577400
Darryl Mok <darryl.mok.2014@smu.edu.sg>
2016-05-31 23:15:01 +0800

    added language list

---
+++ bahamas/src/java/bahamas/dao/LanguageListDAO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Darryl Mok
+ */
+public class LanguageListDAO {
+    
+    private ArrayList<String> languageList;
+    
+
+    public LanguageListDAO() {
+    }
+    
+    public ArrayList<String> retrieveLanguageList() {
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        languageList = new ArrayList<String>();
+
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("SELECT * FROM LANGUAGE_LIST");
+
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                languageList.add(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LanguageListDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve language list from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        return languageList;
+    }
+    
+    
+}
