fc5771fc9c5fbb2551bc83ad16f10daeca1822b4
Darryl Mok <darryl.mok.2014@smu.edu.sg>
2016-06-01 00:08:04 +0800

    added payment mode list

---
+++ bahamas/src/java/bahamas/dao/PaymentModeListDAO.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Darryl Mok
+ */
+public class PaymentModeListDAO {
+    
+    private ArrayList<String> paymentModeList;
+    
+
+    public PaymentModeListDAO() {
+    }
+    
+    public ArrayList<String> retrievePaymentModeList() {
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        paymentModeList = new ArrayList<String>();
+
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("SELECT * FROM PAYMENT_MODE_LIST");
+
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                paymentModeList.add(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PaymentModeListDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve payment mode list from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        return paymentModeList;
+    }
+    
+}
---
+++ bahamas/src/java/bahamas/services/PaymentModeList.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.services;
+
+import bahamas.dao.PaymentModeListDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Darryl Mok
+ */
+@WebServlet(name = "PaymentModeList", urlPatterns = {"/PaymentModeList"})
+public class PaymentModeList extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/JSON;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            JsonObject json = new JsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+            /*          
+             String token = request.getParameter("token");
+             if (!Authenticator.verifyToken(token)) {
+             json.addProperty("message", "invalid token");
+             out.println(gson.toJson(json));
+
+             } else {
+             //Verified
+             */
+            //Validation
+            //Create new contact object
+            PaymentModeListDAO paymentModeListDAO = new PaymentModeListDAO();
+            ArrayList<String> paymentModeList = paymentModeListDAO.retrievePaymentModeList();
+
+            if (!paymentModeList.isEmpty()) {
+                json.addProperty("message", "success");
+
+                JsonArray paymentModeListArray = new JsonArray();
+                JsonObject jsonPaymentModeListArrayObj;
+
+                for (String s : paymentModeList) {
+
+                    jsonPaymentModeListArrayObj = new JsonObject();
+                    jsonPaymentModeListArrayObj.addProperty("paymentMode", s);
+
+                    paymentModeListArray.add(jsonPaymentModeListArrayObj);
+
+                }
+                json.add("paymentModeList", paymentModeListArray);
+                out.println(gson.toJson(json));
+            } else {
+                json.addProperty("message", "failed");
+                out.println(gson.toJson(json));
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
