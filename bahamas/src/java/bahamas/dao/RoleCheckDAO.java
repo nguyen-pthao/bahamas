f9c9c0b2abcfb7201cb39e9f0efb47a1d8dc9004
Tan Si Hao <sihao.tan.2014@smu.edu.sg>
2016-06-06 01:52:00 +0800

    Edited the error found in loading bahamas.sql due to typo and wrong foreign key in Office table. Created RoleCheckDAO, data accuracy is verified. Refer to telegram group chat for screenshot.

---
+++ bahamas/src/java/bahamas/dao/RoleCheckDAO.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.dao;
+
+import bahamas.entity.Contact;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tan.si.hao
+ */
+public class RoleCheckDAO {
+    
+    public static boolean checkRole(int cid, String teamName, String permission){
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("SELECT COUNT(*) AS COUNT FROM TEAM_JOIN WHERE (DATE_OBSOLETE = '0000-00-00' "
+                    + "OR DATE_OBSOLETE = '') AND CONTACT_ID = (?) AND TEAM_NAME = (?) AND PERMISSION = (?) ");
+            stmt.setString(1, Integer.toString(cid));
+            stmt.setString(2, teamName);
+            stmt.setString(3, permission);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                count = Integer.parseInt(rs.getString(1));
+                if(count == 1){
+                    return true;
+                }
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve TEAMJOIN from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        
+        return false;
+    }
+    
+}
