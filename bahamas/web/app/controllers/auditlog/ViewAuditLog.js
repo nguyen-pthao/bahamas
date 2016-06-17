91adbe9f529b84657ad4ba1137d89e25b89c37ab
marcus.ong.2014 <marcus.ong.2014@smu.edu.sg>
2016-06-17 14:09:40 +0800

    omg it's done!!!!!!! audit log filter by timestamp and search and pagination.

--- bahamas/web/app/controllers/auditlog/ViewAuditLog.js
+++ bahamas/web/app/controllers/auditlog/ViewAuditLog.js
@@ -8,9 +8,33 @@
 
 var app = angular.module('bahamas');
 
+app.filter('dateFilter', function () {
+    return function (input, dateStart, dateEnd) {
+        var retArray = [];
+        angular.forEach(input, function (obj) {
+            var receivedDate = obj.Date;
+            var parsedDate = new Date(receivedDate);
+            if (parsedDate >= dateStart && parsedDate <= dateEnd) {
+                retArray.push(obj);
+            }
+        });
+        return retArray;
+    };
+});
+
+//app.filter('startFrom', function () {
+//    return function (input, start) {
+//        if (input) {
+//            start = +start; //parse to int
+//            return input.slice(start);
+//        }
+//        return [];
+//    }
+//});
+
 app.controller('viewAuditLog',
-        ['$scope', '$http', '$location', 'session', '$window', '$state', '$log', 'loadAllAudit',
-            function ($scope, $http, $location, session, $window, $state, $log, loadAllAudit) {
+        ['filterFilter', 'dateFilter', '$scope', '$http', '$location', 'session', '$window', '$state', '$log', 'loadAllAudit',
+            function (filterFilter, dateFilter, $scope, $http, $location, session, $window, $state, $log, loadAllAudit) {
 
                 $scope.backHome = function () {
                     $state.go('admin.homepage');
@@ -19,52 +43,174 @@ app.controller('viewAuditLog',
                 $scope.retrieveAllAuditLog = function () {
                     var auditToRetrieve = {
                         'token': session.getSession('token'),
-                        'cid': angular.fromJson(session.getSession('contact')).cid     
+                        'cid': angular.fromJson(session.getSession('contact')).cid
                     };
                     var allAuditObjKey = [];
                     loadAllAudit.retrieveAllAudit(auditToRetrieve).then(function (response) {
+                        //to get the headers for auditlog
                         $scope.allAuditInfo = response.data.auditlog;
                         var firstAuditObject = $scope.allAuditInfo[0];
                         for (auditHeader in firstAuditObject) {
                             allAuditObjKey.push(auditHeader);
                         }
                         $scope.allAuditObjectKeys = allAuditObjKey;
-                        $scope.totalItems = $scope.allAuditInfo.length;
+                        //end of getting headers for auditlog
+                        $scope.allFiltered = $scope.allAuditInfo;
+                        $scope.totalFilteredItems = $scope.allFiltered.length;
                         $scope.currentPage = 1;
                         $scope.itemsPerPage = $scope.allAuditInfo.length;
-
-                        $scope.itemsPerPageChanged = function () {
-                            if($scope.itemsPerPage == 'toAll'){
-                                $scope.itemsPerPage = $scope.allAuditInfo.length;
-                            }
-                            var total = $scope.totalItems / $scope.itemsPerPage;
-                            $scope.totalPages = Math.ceil(total);
-                            $scope.$watch('currentPage + itemsPerPage', function () {
-                                var begin = ($scope.currentPage - 1) * $scope.itemsPerPage;
-                                var end = begin + parseInt($scope.itemsPerPage);
-                                $scope.filteredAudit = $scope.allAuditInfo.slice(begin, end);
-                            });
-                        };
-                        var total = $scope.totalItems / $scope.itemsPerPage;
-
+                        var total = $scope.totalFilteredItems / $scope.itemsPerPage;
                         $scope.totalPages = Math.ceil(total);
                         $scope.$watch('currentPage + itemsPerPage', function () {
                             var begin = ($scope.currentPage - 1) * $scope.itemsPerPage;
                             var end = begin + parseInt($scope.itemsPerPage);
 
-                            $scope.filteredAudit = $scope.allAuditInfo.slice(begin, end);
+                            $scope.filteredAudit = $scope.allFiltered.slice(begin, end);
                         });
 
-                        $scope.pageChanged = function () {
-                            var total = $scope.totalItems / $scope.itemsPerPage;
+                        $scope.itemsPerPageChanged = function () {
+                            if ($scope.itemsPerPage == 'toAll') {
+                                $scope.itemsPerPage = $scope.allFiltered.length;
+                            }
+                            var newArray = [];
+                            angular.forEach($scope.allFiltered, function (obj) {
+                                var receivedDate = obj.Date;
+                                var parsedDate = new Date(receivedDate);
+                                if (parsedDate >= $scope.dateStart && parsedDate <= $scope.dateEnd) {
+                                    newArray.push(obj);
+                                }
+                            });
+                            $scope.filtered = newArray;
+                            $scope.searchFiltered = filterFilter($scope.filtered, $scope.searchAudit);
+                            $scope.totalFilteredItems = $scope.searchFiltered.length;
+                            var total = $scope.totalFilteredItems / $scope.itemsPerPage;
                             $scope.totalPages = Math.ceil(total);
                             $scope.$watch('currentPage + itemsPerPage', function () {
                                 var begin = ($scope.currentPage - 1) * $scope.itemsPerPage;
                                 var end = begin + parseInt($scope.itemsPerPage);
-
-                                $scope.filteredAudit = $scope.allContactInfo.slice(begin, end);
+                                $scope.filteredAudit = $scope.searchFiltered.slice(begin, end);
                             });
                         };
+
+                        $scope.$watch('dateStart + dateEnd', function () {
+                            var newArray = [];
+                            angular.forEach($scope.allFiltered, function (obj) {
+                                var receivedDate = obj.Date;
+                                var parsedDate = new Date(receivedDate);
+                                if (parsedDate >= $scope.dateStart && parsedDate <= $scope.dateEnd) {
+                                    newArray.push(obj);
+                                }
+                            });
+                            $scope.filtered = newArray;
+                            $scope.searchFiltered = filterFilter($scope.filtered, $scope.searchAudit);
+                            $scope.totalFilteredItems = $scope.searchFiltered.length;
+                            var total = $scope.totalFilteredItems / $scope.itemsPerPage;
+                            $scope.totalPages = Math.ceil(total);
+                            $scope.$watch('currentPage + itemsPerPage', function () {
+                                var begin = ($scope.currentPage - 1) * $scope.itemsPerPage;
+                                var end = begin + parseInt($scope.itemsPerPage);
+                                $scope.filteredAudit = $scope.searchFiltered.slice(begin, end);
+                            });
+                        });
+
+                        $scope.$watch('searchAudit', function (term) {
+                            var newArray = [];
+                            angular.forEach($scope.allFiltered, function (obj) {
+                                var receivedDate = obj.Date;
+                                var parsedDate = new Date(receivedDate);
+                                if (parsedDate >= $scope.dateStart && parsedDate <= $scope.dateEnd) {
+                                    newArray.push(obj);
+                                }
+                            });
+                            $scope.filtered = newArray;
+                            $scope.searchFiltered = filterFilter($scope.filtered, term);
+                            $scope.totalFilteredItems = $scope.searchFiltered.length;
+                            var total = $scope.totalFilteredItems / $scope.itemsPerPage;
+                            $scope.totalPages = Math.ceil(total);
+                            $scope.$watch('currentPage + itemsPerPage', function () {
+                                var begin = ($scope.currentPage - 1) * $scope.itemsPerPage;
+                                var end = begin + parseInt($scope.itemsPerPage);
+                                $scope.filteredAudit = $scope.searchFiltered.slice(begin, end);
+                            });
+                        });
+
+
+
                     });
+
+                    var end = new Date();
+                    end.setHours(23, 59, 59, 999);
+                    var d = null;
+                    $scope.dateStart = d;
+                    $scope.dateEnd = end;
+
+                    $scope.today = function () {
+                        $scope.dt = new Date();
+                    };
+                    $scope.today();
+
+                    $scope.clear = function () {
+                        $scope.dt = null;
+                    };
+
+                    $scope.inlineOptions = {
+                        customClass: getDayClass,
+                        minDate: new Date(),
+                        showWeeks: true
+                    };
+
+                    $scope.dateOptions = {
+                        formatYear: 'yy',
+                        minDate: new Date(),
+                        startingDay: 1
+                    };
+
+                    $scope.toggleMin = function () {
+                        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+                        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+                    };
+
+                    $scope.toggleMin();
+
+                    $scope.open1 = function () {
+                        $scope.popup1.opened = true;
+                    };
+
+                    $scope.open2 = function () {
+                        $scope.popup2.opened = true;
+                    };
+
+                    $scope.setDate = function (year, month, day) {
+                        $scope.dt = new Date(year, month, day);
+                    };
+
+                    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+                    $scope.format = $scope.formats[0];
+                    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+                    $scope.popup1 = {
+                        opened: false
+                    };
+
+                    $scope.popup2 = {
+                        opened: false
+                    };
+                    function getDayClass(data) {
+                        var date = data.date,
+                                mode = data.mode;
+                        if (mode === 'day') {
+                            var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                            for (var i = 0; i < $scope.events.length; i++) {
+                                var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                                if (dayToCheck === currentDay) {
+                                    return $scope.events[i].status;
+                                }
+                            }
+                        }
+
+                        return '';
+                    }
                 };
             }]);
