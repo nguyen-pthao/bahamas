43af6425e80ee9e701e3cb289ffc34022d8ef5ad
marcus.ong.2014 <marcus.ong.2014@smu.edu.sg>
2016-06-22 10:29:45 +0800

    change colour make swee

--- bahamas/web/app/controllers/contact/UserManagement.js
+++ bahamas/web/app/controllers/contact/UserManagement.js
@@ -6,6 +6,13 @@
 
 var app = angular.module('bahamas');
 
-app.controller('userManagement', ['$scope', 'session', '$state', function($scope, session, state){
+app.controller('userManagement', ['$scope', 'session', '$state', function ($scope, session, $state) {
         $scope.ownContact = angular.fromJson(session.getSession('contact'));
-}]);
+
+        var user = session.getSession('userType');
+        var homepage = user + '.homepage';
+
+        $scope.backHome = function () {
+            $state.go(homepage);
+        };
+    }]);
