0e7e905c79b0176af237a949aed4b6d7b58841a1
marcus.ong.2014 <marcus.ong.2014@smu.edu.sg>
2016-07-01 14:41:49 +0800

    change all to chevron up or down.

--- bahamas/web/style/js/app.js
+++ bahamas/web/style/js/app.js
@@ -80,9 +80,9 @@ $.AdminLTE.options = {
   boxWidgetOptions: {
     boxWidgetIcons: {
       //Collapse icon
-      collapse: 'fa-minus',
+      collapse: 'fa-chevron-down',
       //Open icon
-      open: 'fa-plus',
+      open: 'fa-chevron-up',
       //Remove icon
       remove: 'fa-times'
     },
