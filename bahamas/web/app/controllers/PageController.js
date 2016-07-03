8fb87fa8cb600b72f9507c14ec45b370861ce4fc
phuong thao <thalianguyenthao@gmail.com>
2016-07-03 15:31:58 +0800

    Remove usertype and date_created for users

--- bahamas/web/app/controllers/PageController.js
+++ bahamas/web/app/controllers/PageController.js
@@ -14,22 +14,22 @@ app.controller('pageController',
         $scope.populatePage = function () {
             $scope.name = '';
             $scope.userType = '';
-            $scope.dateCreated = '';
+//            $scope.dateCreated = '';
             if (session.getSession('username') != null) {
                 var contact = angular.fromJson(session.getSession('contact'));
-                $scope.name = contact['name'];
-                var dateToParse = contact['date_created'].substring(0, 10);
+                $scope.name = session.getSession('username');
+//                var dateToParse = contact['date_created'].substring(0, 10);
                 $scope.userType = session.getSession('userType');
 
-                var monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-                var dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-                dateToParse = new Date(dateToParse);
-
-                var dateParsed = dateToParse.getDay();
-                var dayParsed = dateToParse.getDate();
-                var monthParsed = dateToParse.getMonth();
-                var yearParsed = dateToParse.getFullYear();
-                $scope.dateCreated = dayArr[dateParsed] + ", " + dayParsed + " " + monthArr[monthParsed] + " " + yearParsed;
+//                var monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//                var dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//                dateToParse = new Date(dateToParse);
+//
+//                var dateParsed = dateToParse.getDay();
+//                var dayParsed = dateToParse.getDate();
+//                var monthParsed = dateToParse.getMonth();
+//                var yearParsed = dateToParse.getFullYear();
+//                $scope.dateCreated = dayArr[dateParsed] + ", " + dayParsed + " " + monthArr[monthParsed] + " " + yearParsed;
             }
         };
 
