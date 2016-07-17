ae26f753a1866108697ccdc3cccd065d479fa08f
Marcus Ong <Mong@Marcuss-MacBook-Pro-2.local>
2016-07-17 15:10:50 +0800

    delete office send back start_office to backend

--- bahamas/web/app/controllers/contact/EditContact.js
+++ bahamas/web/app/controllers/contact/EditContact.js
@@ -1596,6 +1596,7 @@ app.controller('editContact',
                             deleteOffice['contact_id'] = contactToEdit['cid'];
                         }
                         deleteOffice['office_held_name'] = officeHeld['office_held'];
+                        deleteOffice['start_office'] = officeHeld['start_office'];
                         var url = AppAPI.deleteOfficeHeld;
                         deleteService.deleteDataService(deleteOffice, url).then(function (response) {
                             if (response.data.message == 'success') {
