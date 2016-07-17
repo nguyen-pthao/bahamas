8e74c90ca07db0d6c24c367403563c9029edb3e6
Tan Si Hao <sihao.tan.2014@smu.edu.sg>
2016-07-17 16:39:10 +0800

    change donation id to int and fixed officeHeld

--- bahamas/src/java/bahamas/services/RetrieveContactCurrent.java
+++ bahamas/src/java/bahamas/services/RetrieveContactCurrent.java
@@ -647,7 +647,7 @@ public class RetrieveContactCurrent extends HttpServlet {
                 JsonObject jsonDonationObj = new JsonObject();
                 Donation donation = donationList.get(i);
 
-                jsonDonationObj.addProperty("donation_id", Integer.toString(donation.getDonationId()));
+                jsonDonationObj.addProperty("donation_id", donation.getDonationId());
                 if (donation.getDateReceived() != null) {
                     jsonDonationObj.addProperty("date_received", sdf.format(donation.getDateReceived()));
                 } else {
