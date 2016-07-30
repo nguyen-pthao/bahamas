e744778ae873856ea4d524d2dce54cf89fd61359
Tan Si Hao <sihao.tan.2014@smu.edu.sg>
2016-07-30 13:32:49 +0800

    SQL

--- bahamas/sql/data.sql
+++ bahamas/sql/data.sql
@@ -1,149 +1,258 @@
-#Dummy data to be loaded to bahamas tables
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bahamas
+-- ------------------------------------------------------
+-- Server version	5.5.42
 
-use bahamas;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-#PERMISSION_LEVEL_LIST
-insert into PERMISSION_LEVEL_LIST(PERMISSION, DESCRIPTION) value
-('Team manager', 'Manages a team'),
-('Event leader', 'Manages an event'),
-('Associate', 'Joins event');
+--
+-- Dumping data for table `ADDRESS`
+--
 
-#TYPE_OF_CONTACT_LIST
-insert into TYPE_OF_CONTACT_LIST(CONTACT_TYPE) value
-('Individual'),
-('Foundation'),
-('Religious'),
-('Corporate'),
-('Government-linked'),
-('Diplomatic'),
-('Nonprofit local'),
-('Nonprofit abroad'),
-('Medical'),
-('Legal'),
-('Educational'),
-('Media local'),
-('Media abroad'),
-('Financial'),
-('Job agent'),
-('Employer'),
-('Product supplier'),
-('Office services'),
-('Event services'),
-('Financial services'),
-('Maintenance & repair'),
-('TWC2'),
-('Others');
+/*!40000 ALTER TABLE `ADDRESS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ADDRESS` ENABLE KEYS */;
 
-#LANGUAGE_LIST
-insert into LANGUAGE_LIST(LANGUAGE_NAME) value
-('Bengali'),
-('Burmese'),
-('Cambodian'),
-('Chinese'),
-('English'),
-('Filipino'),
-('Hindi'),
-('Indonesian'),
-('Malay'),
-('Punjabi'),
-('Sinhala'),
-('Tamil'),
-('Telugu'),
-('Vietnamese'),
-('Others');
+--
+-- Dumping data for table `APPRECIATION`
+--
 
-#OFFICE_LIST
-insert into OFFICE_LIST(OFFICE_HELD_NAME) value
-('President'),
-('Vice-president'),
-('Secretary'),
-('Treasurer'),
-('Staff');
+/*!40000 ALTER TABLE `APPRECIATION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `APPRECIATION` ENABLE KEYS */;
 
-#MODE_OF_SENDING_RECEIPT_LIST
-insert into MODE_OF_SENDING_RECEIPT_LIST(RECEIPT_MODE_NAME) value
-('Email'),
-('Post'),
-('No address'),
-('Others');
+--
+-- Dumping data for table `AUDITLOG`
+--
 
-#MEMBERSHIP_CLASS_LIST
-insert into MEMBERSHIP_CLASS_LIST(MEMBERSHIP_CLASS_NAME) value
-('Ordinary'),
-('Discounted'),
-('Associate'),
-('Honorary'),
-('Others');
+/*!40000 ALTER TABLE `AUDITLOG` DISABLE KEYS */;
+INSERT INTO `AUDITLOG` VALUES (78,'admin1','2016-07-26 12:41:48','LOGIN - Login into system'),(79,'admin1','2016-07-26 12:42:20','ADD EVENT - Add event under contact: Contact ID: 1 | Event ID: 1'),(80,'admin1','2016-07-26 12:42:36','ADD EVENT ROLES - Add event roles under contact: Contact ID: 1 | Event ID: 1'),(81,'admin1','2016-07-26 15:13:56','LOGIN - Login into system'),(82,'admin1','2016-07-26 16:30:22','LOGIN - Login into system'),(83,'admin1','2016-07-26 16:43:58','LOGIN - Login into system'),(84,'admin1','2016-07-26 16:50:30','LOGIN - Login into system'),(85,'admin1','2016-07-26 17:03:55','ADD EVENT - Add event under contact: Contact ID: 1 | Event ID: 2'),(86,'admin1','2016-07-26 17:04:00','ADD EVENT ROLES - Add event roles under contact: Contact ID: 1 | Event ID: 2'),(87,'admin1','2016-07-26 17:17:33','LOGIN - Login into system'),(88,'admin1','2016-07-26 17:29:31','LOGIN - Login into system'),(89,'admin1','2016-07-26 17:44:35','AUDIT LOG - Access into audit log'),(90,'admin1','2016-07-26 17:44:52','AUDIT LOG - Access into audit log'),(91,'admin1','2016-07-28 23:38:13','LOGIN - Login into system'),(92,'admin1','2016-07-29 12:55:20','LOGIN - Login into system'),(93,'admin1','2016-07-29 12:55:55','CONTACT - Created contact: Contact ID: 9 | Name: Testing'),(94,'admin1','2016-07-29 13:00:05','CONTACT - Created contact: Contact ID: 10 | Name: Pokemon Catcher'),(95,'admin1','2016-07-29 13:00:13','ADD PHONE - Add phone held under contact: Contact ID: 10'),(96,'admin1','2016-07-29 13:17:38','ADD MEMBERSHIP - Add membership under contact: Contact ID: 1'),(97,'admin1','2016-07-29 13:18:02','DELETE MEMBERSHIP - Delete membership under membership: Membership ID: 1'),(98,'admin1','2016-07-29 13:18:37','ADD DONATION - Add donation under contact: Contact ID: 1'),(99,'admin1','2016-07-29 13:22:51','LOGIN - Login into system'),(100,'admin1','2016-07-29 13:52:09','LOGIN - Login into system');
+/*!40000 ALTER TABLE `AUDITLOG` ENABLE KEYS */;
 
-#PAYMENT_MODE_LIST
-insert into PAYMENT_MODE_LIST(PAYMENT_MODE_NAME) value
-('Cash'),
-('Bank direct'),
-('Cheque'),
-('Paypal'),
-('Giving Sg'),
-('Others');
+--
+-- Dumping data for table `CONTACT`
+--
 
-#LSA_CLASS_LIST
-insert into LSA_CLASS_LIST(SKILL_NAME) value
-('Medical'),
-('Physio'),
-('Pharma'),
-('Dental'),
-('Counselling'),
-('Legal'),
-('Pro-bono lawyer'),
-('Media & Comms'),
-('Infotech'),
-('Photo, video & design'),
-('Accounting'),
-('Academia'),
-('Event organising'),
-('Has event space'),
-('Has vehicle'),
-('Has shelter space'),
-('Others');
+/*!40000 ALTER TABLE `CONTACT` DISABLE KEYS */;
+INSERT INTO `CONTACT` VALUES (1,'TWC2','admin1','GwvbMoaGY9yVMnOiMfujehEdmE7jLDXX8DZAM1uUcTw=','1/rqffBv5u4=',1,0,0,'2016-05-24 23:00:00','admin','System Admin','Admin','','IT','TSO','S1234567A','M','Singapore','1977-02-22','','',1);
+/*!40000 ALTER TABLE `CONTACT` ENABLE KEYS */;
 
-#EVENT_CLASS_LIST
-insert into EVENT_CLASS_LIST(EVENT_CLASS_NAME) value
-('Closed'),
-('Open'),
-('Training'),
-('Cancelled'),
-('Others');
+--
+-- Dumping data for table `DONATION`
+--
 
-#EVENT_LOCATION_LIST
-insert into EVENT_LOCATION_LIST(EVENT_LOCATION_NAME) value
-('TWC2 office'),
-('TWC2 DaySpace'),
-('Isthana'),
-('Alankar'),
-('Online'),
-('Others');
+/*!40000 ALTER TABLE `DONATION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `DONATION` ENABLE KEYS */;
 
-#TEAM_AFFILIATION_LIST
-insert into TEAM_AFFILIATION_LIST(TEAM_NAME, DESCRIPTION) value
-('TCRP','This is TCRP team'),
-('R2R','This is R2R team'),
-('FareGo','This is FareGo team'),
-('Roof','This is Roof team'),
-('Lifeline','This is Lifeline team'),
-('Casework','This is Casework team'),
-('Intern','This is Intern team'),
-('Outreach','This is Outreach team'),
-('Discover Sg','This is Discover Sg team'),
-('Public Engagement','This is Public Engagement team'),
-('Communications','This is Communications team'),
-('Research','This is Research team'),
-('Fundraising','This is Fundraising team'),
-('Others','This is Other team');
+--
+-- Dumping data for table `EMAIL`
+--
 
+/*!40000 ALTER TABLE `EMAIL` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EMAIL` ENABLE KEYS */;
 
-#CONTACT
-insert into CONTACT(CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION) value
-(1, 'TWC2', 'admin1', 'GwvbMoaGY9yVMnOiMfujehEdmE7jLDXX8DZAM1uUcTw=','1/rqffBv5u4=', 1, 0, 0, '2016-05-24 23:00:00', 'admin', 'System Admin', 'Admin', '', 'IT', 'TSO', 'S1234567A', 'M', 'Singapore', '1977-02-22', '', '', 1);
+--
+-- Dumping data for table `EVENT`
+--
 
+/*!40000 ALTER TABLE `EVENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EVENT` ENABLE KEYS */;
 
+--
+-- Dumping data for table `EVENT_AFFILIATION`
+--
 
+/*!40000 ALTER TABLE `EVENT_AFFILIATION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EVENT_AFFILIATION` ENABLE KEYS */;
 
+--
+-- Dumping data for table `EVENT_CLASS_LIST`
+--
 
+/*!40000 ALTER TABLE `EVENT_CLASS_LIST` DISABLE KEYS */;
+INSERT INTO `EVENT_CLASS_LIST` VALUES ('Cancelled',0),('Closed',0),('Open',0),('Others',0),('Training',0);
+/*!40000 ALTER TABLE `EVENT_CLASS_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `EVENT_LOCATION_LIST`
+--
+
+/*!40000 ALTER TABLE `EVENT_LOCATION_LIST` DISABLE KEYS */;
+INSERT INTO `EVENT_LOCATION_LIST` VALUES ('Alankar',0),('Isthana',0),('Online',0),('Others',0),('TWC2 DaySpace',0),('TWC2 office',0);
+/*!40000 ALTER TABLE `EVENT_LOCATION_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `EVENT_PARTICIPANT`
+--
+
+/*!40000 ALTER TABLE `EVENT_PARTICIPANT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EVENT_PARTICIPANT` ENABLE KEYS */;
+
+--
+-- Dumping data for table `EVENT_ROLE_ASSIGNMENT`
+--
+
+/*!40000 ALTER TABLE `EVENT_ROLE_ASSIGNMENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `EVENT_ROLE_ASSIGNMENT` ENABLE KEYS */;
+
+--
+-- Dumping data for table `EVENT_STATUS_LIST`
+--
+
+/*!40000 ALTER TABLE `EVENT_STATUS_LIST` DISABLE KEYS */;
+INSERT INTO `EVENT_STATUS_LIST` VALUES ('Cancelled',0),('Filled',0),('Open',0),('Postponed',0);
+/*!40000 ALTER TABLE `EVENT_STATUS_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `LANGUAGE_ASSIGNMENT`
+--
+
+/*!40000 ALTER TABLE `LANGUAGE_ASSIGNMENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `LANGUAGE_ASSIGNMENT` ENABLE KEYS */;
+
+--
+-- Dumping data for table `LANGUAGE_LIST`
+--
+
+/*!40000 ALTER TABLE `LANGUAGE_LIST` DISABLE KEYS */;
+INSERT INTO `LANGUAGE_LIST` VALUES ('Bengali',0),('Burmese',0),('Cambodian',0),('Chinese',0),('English',0),('Filipino',0),('Hindi',0),('Indonesian',0),('Malay',0),('Others',0),('Punjabi',0),('Sinhala',0),('Tamil',0),('Telugu',0),('Vietnamese',0);
+/*!40000 ALTER TABLE `LANGUAGE_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `LSA_CLASS_LIST`
+--
+
+/*!40000 ALTER TABLE `LSA_CLASS_LIST` DISABLE KEYS */;
+INSERT INTO `LSA_CLASS_LIST` VALUES ('Academia',0),('Accounting',0),('Counselling',0),('Dental',0),('Event organising',0),('Has event space',0),('Has shelter space',0),('Has vehicle',0),('Infotech',0),('Legal',0),('Media & Comms',0),('Medical',0),('Others',0),('Pharma',0),('Photo, video & design',0),('Physio',0),('Pro-bono lawyer',0);
+/*!40000 ALTER TABLE `LSA_CLASS_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `MEMBERSHIP`
+--
+
+/*!40000 ALTER TABLE `MEMBERSHIP` DISABLE KEYS */;
+/*!40000 ALTER TABLE `MEMBERSHIP` ENABLE KEYS */;
+
+--
+-- Dumping data for table `MEMBERSHIP_CLASS_LIST`
+--
+
+/*!40000 ALTER TABLE `MEMBERSHIP_CLASS_LIST` DISABLE KEYS */;
+INSERT INTO `MEMBERSHIP_CLASS_LIST` VALUES ('Associate',0),('Discounted',0),('Honorary',0),('Ordinary',0),('Others',0);
+/*!40000 ALTER TABLE `MEMBERSHIP_CLASS_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `MODE_OF_SENDING_RECEIPT_LIST`
+--
+
+/*!40000 ALTER TABLE `MODE_OF_SENDING_RECEIPT_LIST` DISABLE KEYS */;
+INSERT INTO `MODE_OF_SENDING_RECEIPT_LIST` VALUES ('Email',0),('No address',0),('Others',0),('Post',0);
+/*!40000 ALTER TABLE `MODE_OF_SENDING_RECEIPT_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `NOTICE`
+--
+
+/*!40000 ALTER TABLE `NOTICE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `NOTICE` ENABLE KEYS */;
+
+--
+-- Dumping data for table `OFFICE_HELD`
+--
+
+/*!40000 ALTER TABLE `OFFICE_HELD` DISABLE KEYS */;
+/*!40000 ALTER TABLE `OFFICE_HELD` ENABLE KEYS */;
+
+--
+-- Dumping data for table `OFFICE_LIST`
+--
+
+/*!40000 ALTER TABLE `OFFICE_LIST` DISABLE KEYS */;
+INSERT INTO `OFFICE_LIST` VALUES ('President',0),('Secretary',0),('Staff',0),('Treasurer',0),('Vice-president',0);
+/*!40000 ALTER TABLE `OFFICE_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `PAYMENT_MODE_LIST`
+--
+
+/*!40000 ALTER TABLE `PAYMENT_MODE_LIST` DISABLE KEYS */;
+INSERT INTO `PAYMENT_MODE_LIST` VALUES ('Bank direct',0),('Cash',0),('Cheque',0),('Giving Sg',0),('Others',0),('Paypal',0);
+/*!40000 ALTER TABLE `PAYMENT_MODE_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `PERMISSION_LEVEL_LIST`
+--
+
+/*!40000 ALTER TABLE `PERMISSION_LEVEL_LIST` DISABLE KEYS */;
+INSERT INTO `PERMISSION_LEVEL_LIST` VALUES ('Associate','Joins event',0),('Event leader','Manages an event',0),('Team manager','Manages a team',0);
+/*!40000 ALTER TABLE `PERMISSION_LEVEL_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `PHONE`
+--
+
+/*!40000 ALTER TABLE `PHONE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PHONE` ENABLE KEYS */;
+
+--
+-- Dumping data for table `PROXY`
+--
+
+/*!40000 ALTER TABLE `PROXY` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PROXY` ENABLE KEYS */;
+
+--
+-- Dumping data for table `SKILL_ASSIGNMENT`
+--
+
+/*!40000 ALTER TABLE `SKILL_ASSIGNMENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SKILL_ASSIGNMENT` ENABLE KEYS */;
+
+--
+-- Dumping data for table `TEAM_AFFILIATION_LIST`
+--
+
+/*!40000 ALTER TABLE `TEAM_AFFILIATION_LIST` DISABLE KEYS */;
+INSERT INTO `TEAM_AFFILIATION_LIST` VALUES ('Casework','This is Casework team',0),('Communications','This is Communications team',0),('Discover Sg','This is Discover Sg team',0),('FareGo','This is FareGo team',0),('Fundraising','This is Fundraising team',0),('Intern','This is Intern team',0),('Lifeline','This is Lifeline team',0),('Others','This is Other team',0),('Outreach','This is Outreach team',0),('Public Engagement','This is Public Engagement team',0),('R2R','This is R2R team',0),('Research','This is Research team',0),('Roof','This is Roof team',0),('TCRP','This is TCRP team',0);
+/*!40000 ALTER TABLE `TEAM_AFFILIATION_LIST` ENABLE KEYS */;
+
+--
+-- Dumping data for table `TEAM_JOIN`
+--
+
+/*!40000 ALTER TABLE `TEAM_JOIN` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TEAM_JOIN` ENABLE KEYS */;
+
+--
+-- Dumping data for table `TRAINING_CERTIFICATED`
+--
+
+/*!40000 ALTER TABLE `TRAINING_CERTIFICATED` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TRAINING_CERTIFICATED` ENABLE KEYS */;
+
+--
+-- Dumping data for table `TYPE_OF_CONTACT_LIST`
+--
+
+/*!40000 ALTER TABLE `TYPE_OF_CONTACT_LIST` DISABLE KEYS */;
+INSERT INTO `TYPE_OF_CONTACT_LIST` VALUES ('Corporate',0),('Diplomatic',0),('Educational',0),('Employer',0),('Event services',0),('Financial',0),('Financial services',0),('Foundation',0),('Government-linked',0),('Individual',0),('Job agent',0),('Legal',0),('Maintenance & repair',0),('Media abroad',0),('Media local',0),('Medical',0),('Nonprofit abroad',0),('Nonprofit local',0),('Office services',0),('Others',0),('Product supplier',0),('Religious',0),('TWC2',0);
+/*!40000 ALTER TABLE `TYPE_OF_CONTACT_LIST` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-07-29 15:23:50
