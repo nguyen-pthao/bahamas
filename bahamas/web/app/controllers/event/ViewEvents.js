2eee716051eb3240d10770821568dbd70a907529
Marcus Ong <Mong@Marcuss-MacBook-Pro-2.local>
2016-07-30 20:48:27 +0800

    potato power

--- bahamas/web/app/controllers/event/ViewEvents.js
+++ bahamas/web/app/controllers/event/ViewEvents.js
@@ -82,7 +82,43 @@ app.controller('viewEvents',
                       var eventid = event['event_id'];
                       $state.go(url, {eventId: eventid});
                 };
-
+                
+                $scope.deleteEvent = function ($event, event) {
+                            var toURL = user + ".viewEvents";
+                            var eventid = event['event_id'];
+                            
+                            var toDelete = {
+                                'token': session.getSession('token'),
+                                'event_id': eventid
+                            }
+                            ngDialog.openConfirm({
+                                template: './style/ngTemplate/deletePrompt.html',
+                                className: 'ngdialog-theme-default',
+                                scope: $scope
+                            }).then(function (response) {
+                                deleteService.deleteDataService(toDelete, '/event.delete').then(function (response) {
+                                    if (response.data.message === "success") {
+                                        ngDialog.openConfirm({
+                                            template: './style/ngTemplate/deleteSuccess.html',
+                                            className: 'ngdialog-theme-default',
+                                            scope: $scope
+                                        }).then(function (response) {
+                                            $state.reload(toURL);
+                                        })
+                                    } else {
+                                        $scope.error = response.data.message;
+                                        ngDialog.openConfirm({
+                                            template: './style/ngTemplate/deleteFailure.html',
+                                            className: 'ngdialog-theme-default',
+                                            scope: $scope
+                                        }).then(function (response) {
+                                            $state.reload(toURL);
+                                        });
+                                    }
+                                });
+                            });
+                        };
+                
                 $scope.predicate = '';
                 $scope.reverse = true;
 
