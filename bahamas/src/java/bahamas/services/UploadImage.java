bf8f2bbfdfe45edca551ddc5eb0e8cc0f1e2c313
Huxley_Goh <huxley.goh.2014@sis.smu.edu.sg>
2016-07-30 15:20:50 +0800

    Profle pic

---
+++ bahamas/src/java/bahamas/services/UploadImage.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.services;
+
+import bahamas.dao.ContactDAO;
+import bahamas.entity.Contact;
+import bahamas.util.Authenticator;
+import bahamas.util.Validator;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author HUXLEY
+ */
+@WebServlet(name = "UploadImage", urlPatterns = {"/image.upload"})
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+        maxFileSize = 1024 * 1024 * 2, // 
+        maxRequestSize = 1024 * 1024 * 10)   // 
+public class UploadImage extends HttpServlet {
+
+    private static final String SAVE_DIR = "images";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/JSON;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            JsonObject json = new JsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            String token = request.getParameter("token");
+
+            if (token != null) {
+                String username = Authenticator.verifyToken(token);
+                if (username != null) {
+
+                    // gets absolute path of the web application
+                    String appPath = request.getServletContext().getRealPath("");
+                    // constructs path of the directory to save uploaded file
+                    String savePath = appPath + File.separator + SAVE_DIR;
+
+                    // creates the save directory if it does not exists
+                    File fileSaveDir = new File(savePath);
+                    if (!fileSaveDir.exists()) {
+                        fileSaveDir.mkdir();
+                    }
+
+                    //request.getPart("image").write(savePath + File.separator + username + ".jpg");
+                    try {
+                        Files.copy(request.getPart("image").getInputStream(),
+                                new File(savePath + File.separator + username + ".jpg").toPath(), REPLACE_EXISTING);
+                    } catch (Exception e) {
+                        json.addProperty("message", "Image failed to upload!");
+                        out.println(gson.toJson(json));
+                        return;
+                    }
+                    
+                    json.addProperty("message", "Image successfully upload!");
+                    out.println(gson.toJson(json));
+                    return;
+                }
+            }
+            json.addProperty("message", "Image failed to upload!");
+            out.println(gson.toJson(json));
+            return;
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
