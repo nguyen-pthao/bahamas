e32dbe82e1791d13a4edde88431a0c3ab60ae72f
Marcus Ong <Mong@Marcuss-MacBook-Pro-2.local>
2016-08-13 14:50:24 +0800

    fixed global settings.

--- bahamas/web/app/controllers/settings/GlobalSettings.js
+++ bahamas/web/app/controllers/settings/GlobalSettings.js
@@ -7,9 +7,9 @@
 var app = angular.module('bahamas');
 
 app.controller('globalSettings',
-        ['$scope', '$rootScope', '$state', 'session', '$http', 'loadContactType', 'loadEventClass', 'loadEventLocation', 'loadEventStatus', 'loadTeamAffiliation', 'loadPermissionLevel', 'loadLanguage', 'loadLSAClass', 'loadMembershipClass', 'loadPaymentMode', 'loadModeOfSendingReceipt', 'loadOfficeList', 'ngDialog', '$uibModal',
-            function ($scope, $rootScope, $state, session, $http, loadContactType, loadEventClass, loadEventLocation, loadEventStatus, loadTeamAffiliation, loadPermissionLevel, loadLanguage, loadLSAClass, loadMembershipClass, loadPaymentMode, loadModeOfSendingReceipt, loadOfficeList, ngDialog, $uibModal) {
-
+        ['$scope', '$rootScope', '$state', 'session', '$http', 'loadContactType', 'loadEventClass', 'loadEventLocation', 'loadEventStatus', 'loadTeamAffiliation', 'loadPermissionLevel', 'loadLanguage', 'loadLSAClass', 'loadMembershipClass', 'loadPaymentMode', 'loadModeOfSendingReceipt', 'loadOfficeList', 'ngDialog', '$uibModal', 'localStorageService',
+            function ($scope, $rootScope, $state, session, $http, loadContactType, loadEventClass, loadEventLocation, loadEventStatus, loadTeamAffiliation, loadPermissionLevel, loadLanguage, loadLSAClass, loadMembershipClass, loadPaymentMode, loadModeOfSendingReceipt, loadOfficeList, ngDialog, $uibModal, localStorageService) {
+                localStorageService.set('isLocation', false);
                 $scope.backHome = function () {
                     $state.go('admin');
                 };
@@ -18,6 +18,7 @@ app.controller('globalSettings',
                 $scope.getList = function () {
                     $scope.listNotChosen = false;
                     if ($scope.selectedList == 1) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadContactType.retrieveContactType().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.contact, function (value, key) {
@@ -26,6 +27,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 2) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadEventClass.retrieveEventClass().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.eventClassList, function (value, key) {
@@ -34,6 +36,7 @@ app.controller('globalSettings',
                             })
                         })
                     } else if ($scope.selectedList == 3) {
+                        localStorageService.set('isLocation', true);
                         $scope.myPromise = loadEventLocation.retrieveEventLocation().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.eventLocationList, function (value, key) {
@@ -46,6 +49,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 4) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadEventStatus.retrieveEventStatus().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.eventStatusList, function (value, key) {
@@ -54,6 +58,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 5) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadLSAClass.retrieveLSAClass().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.lsaClassList, function (value, key) {
@@ -62,6 +67,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 6) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadLanguage.retrieveLanguage().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.languageList, function (value, key) {
@@ -70,6 +76,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 7) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadMembershipClass.retrieveMembershipClass().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.membershipClassList, function (value, key) {
@@ -78,6 +85,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 8) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadModeOfSendingReceipt.retrieveModeOfSendingReceipt().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.mode, function (value, key) {
@@ -86,6 +94,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 9) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadOfficeList.retrieveOfficeList().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.officeList, function (value, key) {
@@ -94,6 +103,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 10) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadPaymentMode.retrievePaymentMode().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.paymentModeList, function (value, key) {
@@ -102,6 +112,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 11) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadPermissionLevel.retrievePermissionLevel().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.permissionLevelList, function (value, key) {
@@ -110,6 +121,7 @@ app.controller('globalSettings',
                             })
                         });
                     } else if ($scope.selectedList == 12) {
+                        localStorageService.set('isLocation', false);
                         $scope.myPromise = loadTeamAffiliation.retrieveTeamAffiliation().then(function (response) {
                             $rootScope.list = [];
                             angular.forEach(response.data.teamAffiliationList, function (value, key) {
@@ -144,7 +156,6 @@ app.controller('globalSettings',
                         $scope.listToSend['token'] = session.getSession('token');
                         $scope.listToSend['list'] = $rootScope.list;
                         $scope.listToSend['selectedList'] = $scope.selectedList;
-                        console.log($scope.listToSend);
                         //call api to update list in DB. and upon success prompt update success.
                         $http({
                             method: 'POST',
@@ -165,9 +176,18 @@ app.controller('globalSettings',
 
             }]);
 
-app.controller('ModalInstanceCtrl', function ($scope, $rootScope, $uibModalInstance) {
+app.controller('ModalInstanceCtrl', function ($scope, $rootScope, $uibModalInstance, localStorageService) {
     $scope.ok = function () {
-        $rootScope.list.push($scope.input);
+        if (localStorageService.get('isLocation') == true) {
+            var obj = {
+                'location': $scope.input,
+                'address': '',
+                'zipcode': ''
+            }
+            $rootScope.list.push(obj);
+        } else {
+            $rootScope.list.push($scope.input);
+        }
         $uibModalInstance.close();
     };
 
