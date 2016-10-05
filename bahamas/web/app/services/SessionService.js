8af59e0911146bed4625dd6b868b1000fe2925fa
phuong thao <thalianguyenthao@gmail.com>
2016-10-05 15:45:42 +0800

    Change to cookie

--- bahamas/web/app/services/SessionService.js
+++ bahamas/web/app/services/SessionService.js
@@ -10,47 +10,115 @@ app.factory('session', ['$window', function ($window) {
     return {
         setSession: function(key, value) {
             try {
-                if ($window.Storage) {
-                    $window.sessionStorage.setItem(key, value);
-                    return true;
-                } else {
-                    return false;
-                }
+                //if(key == 'token' || key == 'userType' || key == 'username' || key == 'contact_pic' || key == 'contact') {
+                    var line = key;
+                    line += "=";
+                    line += value;
+                    // Internet Explorer 6-11
+                    if(/*@cc_on!@*/false || !!document.documentMode) {
+                        line += "; path=/";
+                    } else {
+                        line += "; expires=0; path=/";
+                    }
+                    document.cookie = line;
+                    //return true;
+                //} else {
+//                    if ($window.Storage) {
+//                        $window.sessionStorage.setItem(key, value);
+//                        return true;
+//                    } else {
+//                        return false;
+//                    }
+//                }
             } catch (error) {
                 window.alert(error, error.message);
             }
         }, 
         getSession: function (key) {
             try {
-                if ($window.Storage) {
-                    return $window.sessionStorage.getItem(key);
-                } else {
-                    return null;
-                }
+                //if(key == 'token' || key == 'userType' || key == 'username' || key == 'contact_pic' || key == 'contact') {
+                    //console.log(document.cookie);
+                    var line = document.cookie;
+                    //console.log(key);
+                    var keyToFind = (key += "=");
+                    //console.log(keyToFind);
+                    var indexNumber = line.indexOf(keyToFind);
+                    //console.log(indexNumber);
+                    if(indexNumber != -1) {
+                        indexNumber += keyToFind.length;
+                        //console.log(indexNumber);
+                        //console.log(line.substring(indexNumber, line.indexOf(";", indexNumber)));
+                        var indexNumber2 = line.indexOf(";", indexNumber);
+                        var toReturn = '';
+                        if (indexNumber2 != -1) {
+                            toReturn = line.substring(indexNumber, indexNumber2);
+                        } else {
+                            toReturn = line.substring(indexNumber, line.length);
+                        }
+                        //console.log(toReturn);
+                        return toReturn;
+                        //return line.substring(indexNumber, line.indexOf(";", indexNumber));
+                    } else {
+                        return null;
+                    }
+                    //return document.cookie;
+//                } else {
+//                    if ($window.Storage) {
+//                        return $window.sessionStorage.getItem(key);
+//                    } else {
+//                        return null;
+//                    }
+//                }
             } catch (error) {
                 window.alert(error, error.message);
             }
         },
         removeKey: function (key) {
             try {
-                if($window.Storage) {
-                    $window.sessionStorage.removeItem(key);
+                //if(key == 'token' || key == 'userType' || key == 'username' || key == 'contact_pic') {
+                var line = document.cookie;
+                var indexNumber = line.indexOf(key);
+                if(indexNumber != -1) {
+                    key += '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
+                    document.cookie = key;
                     return true;
                 } else {
                     return false;
-                }
+                }    
+                    //return true;
+//                } else {
+//                    if($window.Storage) {
+//                        $window.sessionStorage.removeItem(key);
+//                        return true;
+//                    } else {
+//                        return false;
+//                    }
+//                }
             } catch (error) {
                 window.alert(error, error.message);
             }  
         },
         terminateSession: function(){
             try {
-                if ($window.Storage) {
-                    $window.sessionStorage.clear();
-                    return true;
-                } else {
-                    return false;
+//                var cookiesStored = document.cookie.split(";");
+//                for(var cookie in cookiesStored) {
+//                    document.cookie
+//                }
+//                if ($window.Storage) {
+//                    $window.sessionStorage.clear();
+//                    return true;
+//                } else {
+//                    return false;
+//                }
+                var cookieArray = document.cookie.split(";");
+                for(var i in cookieArray) {
+                    var cookie = cookieArray[i];
+                    var keyIndex = cookie.indexOf("=");
+                    var key = cookie.substring(0, keyIndex);
+                    key += '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
+                    document.cookie = key;
                 }
+                return true;
             } catch (error) {
                 window.alert(error, error.message);
             }
