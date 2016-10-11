c42b233a7c96b8174dff6b5f659a30bc08378931
Tan Si Hao <sihao.tan.2014@smu.edu.sg>
2016-10-12 00:58:18 +0800

    Search Contact

---
+++ bahamas/src/java/bahamas/dao/SearchContactDAO.java
@@ -0,0 +1,337 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.dao;
+
+import bahamas.entity.Contact;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tan.si.hao
+ */
+public class SearchContactDAO {
+
+    private static SimpleDateFormat datetime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    
+    public static HashMap<Integer, Contact> searchContactByNameAltnameNationality(String nameValue, String altNameValue, String nationalityValue) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        HashMap<Integer, Contact> contactHM = new HashMap<Integer, Contact>();
+        
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("Select CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,"
+                    + "DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,"
+                    + "JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION "
+                    + "FROM CONTACT WHERE NAME LIKE ? AND ALT_NAME LIKE ? AND NATIONALITY LIKE ?");
+            
+            if(nameValue == null){
+                stmt.setString(1, "%");
+            }else{
+                stmt.setString(1, "%"+nameValue+"%");
+            }
+            if(altNameValue == null){
+                stmt.setString(2, "%");
+            }else{
+                stmt.setString(2, "%"+altNameValue+"%");
+            }
+            if(nationalityValue == null){
+                stmt.setString(3, "%");
+            }else{
+                stmt.setString(3, "%"+nationalityValue+"%");
+            }
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int contactId = Integer.parseInt(rs.getString(1));
+                String contactType = rs.getString(2);
+                String username = rs.getString(3);
+                String password = rs.getString(4);
+                String salt = rs.getString(5);
+                boolean isAdmin = rs.getBoolean(6);
+                boolean isNovice = rs.getBoolean(7);
+                boolean deactivated = rs.getBoolean(8);
+                String dateStr = rs.getString(9);
+                Date dateCreated = datetime.parse(dateStr);
+                String createdBy = rs.getString(10);
+                String name = rs.getString(11);
+                String altName = rs.getString(12);
+                String explainIfOther = rs.getString(13);
+                String profession = rs.getString(14);
+                String jobTitle = rs.getString(15);
+                String nric = rs.getString(16);
+                String gender = rs.getString(17);
+                String nationality = rs.getString(18);
+                Date dateOfBirth = rs.getDate(19);
+                String profilePic = rs.getString(20);
+                String remarks = rs.getString(21);
+                boolean notification = rs.getBoolean(22);
+
+                Contact contact = new Contact(contactId, contactType, username, password, salt, isAdmin, isNovice, deactivated, dateCreated, createdBy, name, altName, explainIfOther, profession, jobTitle, nric, gender, nationality, dateOfBirth, profilePic, remarks, notification);
+                contactHM.put(contactId,contact);
+
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve contact from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return contactHM;
+    }
+    
+    public static HashMap<Integer, Contact> searchContactByTeam(String team) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        HashMap<Integer, Contact> contactHM = new HashMap<Integer, Contact>();
+        
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("Select CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,"
+                    + "DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,"
+                    + "JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION "
+                    + "FROM CONTACT WHERE CONTACT_ID IN ( SELECT DISTINCT CONTACT_ID FROM TEAM_JOIN WHERE TEAM_NAME = ? AND PERMISSION IS NOT NULL AND IF(DATE_OBSOLETE IS NOT NULL,DATE_OBSOLETE >= CURDATE(),TRUE))");
+
+            stmt.setString(1, team);
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int contactId = Integer.parseInt(rs.getString(1));
+                String contactType = rs.getString(2);
+                String username = rs.getString(3);
+                String password = rs.getString(4);
+                String salt = rs.getString(5);
+                boolean isAdmin = rs.getBoolean(6);
+                boolean isNovice = rs.getBoolean(7);
+                boolean deactivated = rs.getBoolean(8);
+                String dateStr = rs.getString(9);
+                Date dateCreated = datetime.parse(dateStr);
+                String createdBy = rs.getString(10);
+                String name = rs.getString(11);
+                String altName = rs.getString(12);
+                String explainIfOther = rs.getString(13);
+                String profession = rs.getString(14);
+                String jobTitle = rs.getString(15);
+                String nric = rs.getString(16);
+                String gender = rs.getString(17);
+                String nationality = rs.getString(18);
+                Date dateOfBirth = rs.getDate(19);
+                String profilePic = rs.getString(20);
+                String remarks = rs.getString(21);
+                boolean notification = rs.getBoolean(22);
+
+                Contact contact = new Contact(contactId, contactType, username, password, salt, isAdmin, isNovice, deactivated, dateCreated, createdBy, name, altName, explainIfOther, profession, jobTitle, nric, gender, nationality, dateOfBirth, profilePic, remarks, notification);
+                contactHM.put(contactId,contact);
+
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve contact from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return contactHM;
+    }
+    
+    public static HashMap<Integer, Contact> searchContactByAppreciationGesture(String appreciationGesture) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        HashMap<Integer, Contact> contactHM = new HashMap<Integer, Contact>();
+        
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("Select CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,"
+                    + "DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,"
+                    + "JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION "
+                    + "FROM CONTACT WHERE CONTACT_ID IN ( SELECT DISTINCT CONTACT_ID FROM APPRECIATION WHERE APPRECIATION_GESTURE LIKE ?)");
+
+            stmt.setString(1, "%"+appreciationGesture+"%");
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int contactId = Integer.parseInt(rs.getString(1));
+                String contactType = rs.getString(2);
+                String username = rs.getString(3);
+                String password = rs.getString(4);
+                String salt = rs.getString(5);
+                boolean isAdmin = rs.getBoolean(6);
+                boolean isNovice = rs.getBoolean(7);
+                boolean deactivated = rs.getBoolean(8);
+                String dateStr = rs.getString(9);
+                Date dateCreated = datetime.parse(dateStr);
+                String createdBy = rs.getString(10);
+                String name = rs.getString(11);
+                String altName = rs.getString(12);
+                String explainIfOther = rs.getString(13);
+                String profession = rs.getString(14);
+                String jobTitle = rs.getString(15);
+                String nric = rs.getString(16);
+                String gender = rs.getString(17);
+                String nationality = rs.getString(18);
+                Date dateOfBirth = rs.getDate(19);
+                String profilePic = rs.getString(20);
+                String remarks = rs.getString(21);
+                boolean notification = rs.getBoolean(22);
+
+                Contact contact = new Contact(contactId, contactType, username, password, salt, isAdmin, isNovice, deactivated, dateCreated, createdBy, name, altName, explainIfOther, profession, jobTitle, nric, gender, nationality, dateOfBirth, profilePic, remarks, notification);
+                contactHM.put(contactId,contact);
+
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve contact from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return contactHM;
+    }
+    
+    public static HashMap<Integer, Contact> searchContactByLanguageName(String languageName) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        HashMap<Integer, Contact> contactHM = new HashMap<Integer, Contact>();
+        
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("Select CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,"
+                    + "DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,"
+                    + "JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION "
+                    + "FROM CONTACT WHERE CONTACT_ID IN ( SELECT DISTINCT CONTACT_ID FROM LANGUAGE_ASSIGNMENT WHERE LANGUAGE_NAME = ? AND IF(DATE_OBSOLETE IS NOT NULL,DATE_OBSOLETE >= CURDATE(),TRUE))");
+
+            stmt.setString(1, languageName);
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int contactId = Integer.parseInt(rs.getString(1));
+                String contactType = rs.getString(2);
+                String username = rs.getString(3);
+                String password = rs.getString(4);
+                String salt = rs.getString(5);
+                boolean isAdmin = rs.getBoolean(6);
+                boolean isNovice = rs.getBoolean(7);
+                boolean deactivated = rs.getBoolean(8);
+                String dateStr = rs.getString(9);
+                Date dateCreated = datetime.parse(dateStr);
+                String createdBy = rs.getString(10);
+                String name = rs.getString(11);
+                String altName = rs.getString(12);
+                String explainIfOther = rs.getString(13);
+                String profession = rs.getString(14);
+                String jobTitle = rs.getString(15);
+                String nric = rs.getString(16);
+                String gender = rs.getString(17);
+                String nationality = rs.getString(18);
+                Date dateOfBirth = rs.getDate(19);
+                String profilePic = rs.getString(20);
+                String remarks = rs.getString(21);
+                boolean notification = rs.getBoolean(22);
+
+                Contact contact = new Contact(contactId, contactType, username, password, salt, isAdmin, isNovice, deactivated, dateCreated, createdBy, name, altName, explainIfOther, profession, jobTitle, nric, gender, nationality, dateOfBirth, profilePic, remarks, notification);
+                contactHM.put(contactId,contact);
+
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve contact from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return contactHM;
+    }
+    
+    public static HashMap<Integer, Contact> searchContactBySkillName(String skillName) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        HashMap<Integer, Contact> contactHM = new HashMap<Integer, Contact>();
+        
+        try {
+            conn = ConnectionManager.getConnection();
+            stmt = conn.prepareStatement("Select CONTACT_ID, CONTACT_TYPE,USERNAME,PASSWORD,SALT,ISADMIN,ISNOVICE,"
+                    + "DEACTIVATED,DATE_CREATED,CREATED_BY,NAME,ALT_NAME,EXPLAIN_IF_OTHER,PROFESSION,"
+                    + "JOB_TITLE,NRIC_FIN,GENDER,NATIONALITY,DATE_OF_BIRTH,PROFILE_PIC,REMARKS,NOTIFICATION "
+                    + "FROM CONTACT WHERE CONTACT_ID IN ( SELECT DISTINCT CONTACT_ID FROM SKILL_ASSIGNMENT WHERE SKILL_NAME = ? AND IF(DATE_OBSOLETE IS NOT NULL,DATE_OBSOLETE >= CURDATE(),TRUE) )");
+
+            stmt.setString(1, skillName);
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+
+                int contactId = Integer.parseInt(rs.getString(1));
+                String contactType = rs.getString(2);
+                String username = rs.getString(3);
+                String password = rs.getString(4);
+                String salt = rs.getString(5);
+                boolean isAdmin = rs.getBoolean(6);
+                boolean isNovice = rs.getBoolean(7);
+                boolean deactivated = rs.getBoolean(8);
+                String dateStr = rs.getString(9);
+                Date dateCreated = datetime.parse(dateStr);
+                String createdBy = rs.getString(10);
+                String name = rs.getString(11);
+                String altName = rs.getString(12);
+                String explainIfOther = rs.getString(13);
+                String profession = rs.getString(14);
+                String jobTitle = rs.getString(15);
+                String nric = rs.getString(16);
+                String gender = rs.getString(17);
+                String nationality = rs.getString(18);
+                Date dateOfBirth = rs.getDate(19);
+                String profilePic = rs.getString(20);
+                String remarks = rs.getString(21);
+                boolean notification = rs.getBoolean(22);
+
+                Contact contact = new Contact(contactId, contactType, username, password, salt, isAdmin, isNovice, deactivated, dateCreated, createdBy, name, altName, explainIfOther, profession, jobTitle, nric, gender, nationality, dateOfBirth, profilePic, remarks, notification);
+                contactHM.put(contactId,contact);
+
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchContactDAO.class.getName()).log(Level.SEVERE, "Unable to retrieve contact from database data", ex);
+            ex.printStackTrace();
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+
+        return contactHM;
+    }
+    
+}
---
+++ bahamas/src/java/bahamas/services/SearchContact.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bahamas.services;
+
+import bahamas.dao.ContactDAO;
+import bahamas.dao.SearchContactDAO;
+import bahamas.entity.Contact;
+import bahamas.util.Authenticator;
+import bahamas.util.Validator;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author tan.si.hao
+ */
+@WebServlet(name = "SearchContact", urlPatterns = {"/contact.search"})
+public class SearchContact extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            JsonObject json = new JsonObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            //Retrieve the json string as a reader 
+            StringBuilder sb = new StringBuilder();
+            try {
+                BufferedReader reader = request.getReader();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (Exception e) {
+                json.addProperty("message", "fail");
+                out.println(gson.toJson(json));
+                return;
+            }
+
+            String jsonLine = sb.toString();
+            if (jsonLine == null || jsonLine.isEmpty()) {
+                json.addProperty("message", "fail");
+                out.println(gson.toJson(json));
+
+            } else {
+                JsonElement jelement = new JsonParser().parse(jsonLine);
+                JsonObject jobject = jelement.getAsJsonObject();
+                String token = Validator.containsBlankField(jobject.get("token"));
+                String name = Validator.containsBlankField(jobject.get("name"));
+                String altname = Validator.containsBlankField(jobject.get("altname"));
+                String nationality = Validator.containsBlankField(jobject.get("nationality"));
+                String team = Validator.containsBlankField(jobject.get("team"));
+                String appreciation = Validator.containsBlankField(jobject.get("appreciation"));
+                String language = Validator.containsBlankField(jobject.get("language"));
+                String skill = Validator.containsBlankField(jobject.get("skill"));
+                String username = Authenticator.verifyToken(token);
+                if (username == null) {
+                    json.addProperty("message", "invalid token");
+                    out.println(gson.toJson(json));
+                } else {
+                    ContactDAO cDAO = new ContactDAO();
+                    Contact contact = cDAO.retrieveContactByUsername(username);
+                    if (contact.isIsAdmin()) {
+                        HashMap<Integer, Contact> contactHM = null;
+                        if (name != null || altname != null || nationality != null) {
+                            contactHM = SearchContactDAO.searchContactByNameAltnameNationality(name, altname, nationality);
+                        }
+                        if (contactHM == null && team != null) {
+                            contactHM = SearchContactDAO.searchContactByTeam(team);
+                        } else if (contactHM != null && team != null) {
+                            HashMap<Integer, Contact> tempHM = SearchContactDAO.searchContactByTeam(team);
+                            contactHM.keySet().retainAll(tempHM.keySet());
+                        }
+                        if (contactHM == null && appreciation != null) {
+                            contactHM = SearchContactDAO.searchContactByAppreciationGesture(appreciation);
+                        } else if (contactHM != null && appreciation != null) {
+                            HashMap<Integer, Contact> tempHM = SearchContactDAO.searchContactByAppreciationGesture(appreciation);
+                            contactHM.keySet().retainAll(tempHM.keySet());
+                        }
+                        if (contactHM == null && language != null) {
+                            contactHM = SearchContactDAO.searchContactByLanguageName(language);
+                        } else if (contactHM != null && language != null) {
+                            HashMap<Integer, Contact> tempHM = SearchContactDAO.searchContactByLanguageName(language);
+                            contactHM.keySet().retainAll(tempHM.keySet());
+                        }
+                        if (contactHM == null && skill != null) {
+                            contactHM = SearchContactDAO.searchContactBySkillName(skill);
+                        } else if (contactHM != null && skill != null) {
+                            HashMap<Integer, Contact> tempHM = SearchContactDAO.searchContactBySkillName(skill);
+                            contactHM.keySet().retainAll(tempHM.keySet());
+                        }
+                        json.addProperty("message", "success");
+                        JsonArray contactArray = new JsonArray();
+                        JsonObject jsonContactObj;
+                        for (int tempContactId : contactHM.keySet()) {
+                            Contact tempContact = contactHM.get(tempContactId);
+                            String tempUsername = "No username";
+                            if(tempContact.getUsername() != null){
+                                tempUsername = tempContact.getUsername();
+                            }
+                            jsonContactObj = new JsonObject();
+                            jsonContactObj.addProperty("contactid", tempContact.getContactId());
+                            jsonContactObj.addProperty("name", tempContact.getName() + " (" + tempUsername + ")");
+                            contactArray.add(jsonContactObj);
+                        }
+                        json.add("contact", contactArray);
+                        out.println(gson.toJson(json));
+                    } else {
+                        json.addProperty("message", "fail");
+                        out.println(gson.toJson(json));
+                    }
+                }
+
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
