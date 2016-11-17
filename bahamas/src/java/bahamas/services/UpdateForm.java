84821eb364038dff16e09bc8b25c93ce0b698864
Huxley_Goh <huxley.goh.2014@sis.smu.edu.sg>
2016-11-17 10:43:25 +0800

    Fix json errors

--- bahamas/src/java/bahamas/services/UpdateForm.java
+++ bahamas/src/java/bahamas/services/UpdateForm.java
@@ -47,7 +47,7 @@ public class UpdateForm extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/JSON;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             JsonObject json = new JsonObject();
